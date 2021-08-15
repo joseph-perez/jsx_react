717d90c99b44a4ecfb8385bb3f3de39e1eb616c4
Joseph Perez <josephperez717@gmail.com>
2021-08-14 23:36:39 -0600

    setting up App.js to render the React app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <div>
+      <h1>Puppies!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
