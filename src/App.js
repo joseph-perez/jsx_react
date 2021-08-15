432e02207eabc6fc8e3de6a79e801912b75450ae
Joseph Perez <josephperez717@gmail.com>
2021-08-14 23:41:21 -0600

    creating a puppy component with props and using it in the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Puppy = (props) => {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h3>Breed: {props.breed}</h3>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
       <h1>Puppies!</h1>
+      <Puppy name="Coco" breed="Chihuahua" />
+      <Puppy name="Donut" breed="The best breed :)" />
+      <Puppy name="Daisy" breed="Beagle" />
     </div>
   );
 };
