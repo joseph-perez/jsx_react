ab73c2a3da16f3e78dda0bd804896502ce54c214
Joseph Perez <josephperez717@gmail.com>
2021-08-18 20:07:05 -0600

    getting the puppies functional component to fetch a puppy! :dog:
    Using hooks in this functional component. Using useState to setup the state for the puppyImages array. Also using useEffect to make the request and then set the puppy images in state.

--- src/PuppiesFunctionalComponent.js
+++ src/PuppiesFunctionalComponent.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from 'react';
+
 function PuppiesFunctionalComponent() {
-  return <h2>No puppies in functional component 🐶 😖</h2>;
+  const [puppyImages, setPuppyImages] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random/1')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPuppyImages(data.message);
+      });
+  }, []);
+
+  return (
+    <>
+      {puppyImages.length === 0 ? (
+        <h2>No puppies in functional component 🐶 😖</h2>
+      ) : (
+        puppyImages.map((puppyImg) => (
+          <img
+            key={puppyImg}
+            style={{ width: 200, height: 200 }}
+            src={puppyImg}
+            alt="a puppy"
+          />
+        ))
+      )}
+    </>
+  );
 }
 
 export default PuppiesFunctionalComponent;
