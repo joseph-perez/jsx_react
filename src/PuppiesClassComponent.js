4fa1f2c6726125abbdd209c65fa118e3f6be5470
Joseph Perez <josephperez717@gmail.com>
2021-08-18 19:49:09 -0600

    getting the puppies class component to fetch an image of a puppy! :dog:

--- src/PuppiesClassComponent.js
+++ src/PuppiesClassComponent.js
@@ -4,11 +4,33 @@ class PuppiesClassComponent extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      puppyImages: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/image/random/1')
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ puppyImages: data.message });
+      });
   }
 
   render() {
-    return <h2>No puppies in class component 🐶 😖</h2>;
+    return (
+      <>
+        <h2>No puppies in class component 🐶 😖</h2>
+        {this.state.puppyImages.map((puppyImg) => (
+          <img
+            key={puppyImg}
+            style={{ width: 200, height: 200 }}
+            src={puppyImg}
+            alt="a puppy"
+          />
+        ))}
+      </>
+    );
   }
 }
 
